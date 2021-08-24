2723cdfa1468934bd037070038e2d72613481281
zimmeste <Stefan.Zimmermann@infineon.com>
2021-08-24 17:18:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using Infineon.Yoda;
+
+namespace YodaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            String yodaChannel = "RBG_T_TEST";
+            String remotePath = @"\\mitconfig.rbg.infineon.com\yodaconfig\mc2uc";
+            String hostType = "SERVER";
+            String subject = "RBG_T.PCS.EM.TEST";
+
+            Console.WriteLine("Yoda Channel: " + yodaChannel);
+            Console.WriteLine("Remote Path: " + remotePath);
+            Console.WriteLine("Host Type: " + hostType);
+            Console.WriteLine("Subject: " + subject);
+
+            IfxDoc message = new IfxDoc();
+            message.Add("Application", "Infineon Cloud Platform");
+            message.Add("Message", "Hello World");
+
+            while (true)
+            {
+                Publish(message, yodaChannel, remotePath, hostType, subject, 10);
+                Console.WriteLine("Message sent to: " + subject);
+                //i++;
+                //Console.WriteLine(i + ": Hello World!");
+                Thread.Sleep(2000);
+            }
+        }
+
+        public static void Publish(IfxDoc message, String yodaChannel, String remotePath, String hostType, String subject, int timeout)
+        {
+            IfxDoc config = new IfxDoc();
+            config.Add(IfxConst.IFX_CHANNEL, yodaChannel);
+            config.Add(IfxConst.IFX_CONFIGURATION_LOCATION, remotePath);
+            config.Add("CTX HOSTTYPE", hostType);
+
+            IfxTransport EMTransport = new IfxTransport(config);
+            EMTransport.FaultToleranceWeight = 1;
+
+            message.SendSubject = subject;
+
+            // Send message
+            EMTransport.Publish(message, IfxConst.IfxPublishMode.Default, timeout);
+        }
+    }
+}
